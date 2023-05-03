392e6c113935d075383dbe83f6f78d33505a8a81
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2023-05-03 16:22:05 +0530

    code changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Rushikesh and Vaishnavi
         </a>
       </header>
     </div>
