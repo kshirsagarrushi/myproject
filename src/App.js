382d88a7a8282ceb5dee28eafdbdd4a79f3c8b57
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2023-05-03 16:47:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Rushikesh and Vaishnavi or Pankaj and Rutuja
+          Learn React with Rushikesh and Vaishnavi or Pankaj and Rutuja and om 
         </a>
       </header>
     </div>
