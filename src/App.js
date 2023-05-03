13b480a0e15ad1b32a703163bd5f9b8787c6beab
kshirsagarrushi <108231382+kshirsagarrushi@users.noreply.github.com>
2023-05-03 16:30:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Rushikesh and Vaishnavi
+          Learn React with Rushikesh and Vaishnavi or Pankaj and Rutuja
         </a>
       </header>
     </div>
